9654a5ceca49d8fa40b2feea9ee10e1c6f646f3d
Nathanael Ang <natnatang@hotmail.com>
2024-01-24 15:01:09 +1000

    Add Bootstrap, Font Awesome, and ngx-bootstrap dependencies Enable CORS and HTTPS redirection in API Import HttpClientModule and BrowserAnimationsModule in app.module.ts Update styles and scripts in angular.json Update app.component.ts to fetch users from API Update package-lock.json with new dependencies

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
